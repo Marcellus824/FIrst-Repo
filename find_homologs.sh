9bbf24cdb0eb959509ec148b928ddfdb1a88d791
Marcellus824 <166552054+Marcellus824@users.noreply.github.com>
2026-09-20 18:30:19 -0400

    File for BIOL7200 question 3 excersize 3

---
+++ find_homologs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#Shebang intialization
+
+#Match Protein to nucleotides with a database search
+tblastn -query $1 -db $2 -out $3 \
+#filters the output for match length >90%
+-outfmt 6 -qcov_hsp_perc 90 \
+
+#filters the only include match identifiers >30%
+cp $3 preface.txt
+awk '$3 > 30' preface.txt > $3
+
+
+
+#Shows how many matches were found after the filters 
+wc -l $3
