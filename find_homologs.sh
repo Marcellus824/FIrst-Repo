c13c9c5450ca09c9f30856515f6c86ecb26408cd
Marcellus824 <166552054+Marcellus824@users.noreply.github.com>
2026-09-21 16:17:49 -0400

    Modify tblastn command to enhance output filtering
    Updated tblastn command to include output format and query coverage percentage on the same line as the initial tblastn

--- find_homologs.sh
+++ find_homologs.sh
@@ -2,9 +2,8 @@
 #Shebang intialization
 
 #Match Protein to nucleotides with a database search
-tblastn -query $1 -db $2 -out $3 \
+tblastn -query $1 -db $2 -out $3 -outfmt 6 -qcov_hsp_perc 90 
 #filters the output for match length >90%
--outfmt 6 -qcov_hsp_perc 90 \
 
 #filters the only include match identifiers >30%
 cp $3 preface.txt
