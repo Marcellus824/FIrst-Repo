e0375d0faba6da0ba462eede9198fb4ead6a272f
Marcellus824 <166552054+Marcellus824@users.noreply.github.com>
2026-09-21 22:02:37 -0400

    Fix tblastn command to use subject instead of db
    Changed database search from '-db' to '-subject' in tblastn command.

--- find_homologs.sh
+++ find_homologs.sh
@@ -2,7 +2,7 @@
 #Shebang intialization
 
 #Match Protein to nucleotides with a database search
-tblastn -query $1 -db $2 -out $3 -outfmt 6 -qcov_hsp_perc 90 
+tblastn -query $1 -subject $2 -out $3 -outfmt '6' -qcov_hsp_perc 90 
 #filters the output for match length >90%
 
 #filters the only include match identifiers >30%
